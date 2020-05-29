c41e976d2c6e9fb57b77dfc02ed64212409c8d8a
kadocho12 <baseballtaichi511218@gmail.com>
2020-05-29 15:02:35 +0900

    タスク一覧コンポーネント

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
+import VueRouter from 'vue-router';
 import HeaderComponent from './components/HeaderComponent';
+import TaskListComponent from './components/TaskListComponent';
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -30,6 +32,20 @@ Vue.component('header-component', HeaderComponent);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
+
 const app = new Vue({
     el: '#app',
+    router
 });
