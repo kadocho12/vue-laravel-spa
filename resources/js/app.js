f33068d1006f2ecdd64dc43eef5cf66f1fb71093
kadocho12 <baseballtaichi511218@gmail.com>
2020-05-29 18:17:37 +0900

    タスク登録コンポーネント実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router';
 import HeaderComponent from './components/HeaderComponent';
 import TaskListComponent from './components/TaskListComponent';
+import TaskCreateComponent from './components/TaskCreateComponent';
 import TaskShowComponent from './components/TaskShowComponent';
 
 /**
@@ -42,6 +43,11 @@ const router = new VueRouter({
             path: '/tasks',
             name: 'task.list',
             component: TaskListComponent
+        },
+        {
+            path: '/tasks/create',
+            name: 'task.create',
+            component: TaskCreateComponent
         },
         {
             path: '/tasks/:taskId',
