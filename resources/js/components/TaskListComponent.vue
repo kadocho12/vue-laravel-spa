90be0d362b40820ae291102b2c5e806606676670
kadocho12 <baseballtaichi511218@gmail.com>
2020-05-30 15:26:24 +0900

    コメント削除

--- resources/js/components/TaskListComponent.vue
+++ resources/js/components/TaskListComponent.vue
@@ -13,63 +13,6 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <!-- <th scope="row">1</th>
-          <td>Title1</td>
-          <td>Content1</td>
-          <td>Ichiro</td>
-          <td>
-            <router-link v-bind:to="{ name:'task.show', params:{taskId:1} }">
-              <button class="btn btn-primary">Show</button>
-            </router-link>
-          </td>
-          <td>
-            <router-link v-bind:to="{ name:'task.edit', params:{taskId:1} }">
-              <button class="btn btn-success">Edit</button>
-            </router-link>
-          </td>
-          <td>
-            <button class="btn btn-danger">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Title2</td>
-          <td>Content2</td>
-          <td>Jiro</td>
-          <td>
-            <router-link v-bind:to="{ name:'task.show', params:{taskId:2} }">
-              <button class="btn btn-primary">Show</button>
-            </router-link>
-          </td>
-          <td>
-            <router-link v-bind:to="{ name:'task.edit', params:{taskId:2} }">
-              <button class="btn btn-success">Edit</button>
-            </router-link>
-          </td>
-          <td>
-            <button class="btn btn-danger">Delete</button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Title3</td>
-          <td>Content3</td>
-          <td>Saburo</td>
-          <td>
-            <router-link v-bind:to="{ name:'task.show', params:{taskId:3} }">
-              <button class="btn btn-primary">Show</button>
-            </router-link>
-          </td>
-          <td>
-            <router-link v-bind:to="{ name:'task.edit', params:{taskId:3} }">
-              <button class="btn btn-success">Edit</button>
-            </router-link>
-          </td>
-          <td>
-            <button class="btn btn-danger">Delete</button>
-          </td>
-        </tr> -->
         <tr v-for="task in tasks" v-bind:key="{params: {taskId: task.id}}">
           <th scope="row" class="test">{{ task.id }}</th>
           <td>{{ task.title }}</td>
