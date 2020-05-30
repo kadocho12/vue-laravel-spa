63393f6bbad3cd228492d97798b0e739689143d1
kadocho12 <baseballtaichi511218@gmail.com>
2020-05-30 10:41:17 +0900

    タスク登録API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,4 +15,9 @@ class TaskController extends Controller
     {
         return $task;
     }
+
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
 }
